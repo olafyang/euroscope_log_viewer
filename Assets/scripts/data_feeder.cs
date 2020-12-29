3388be3a4158ac7abcd8ac8de223db1b62a397e6
Olaf Yang <ian9703@gmail.com>
2020-12-29 23:04:17 +0100

    init

---
+++ Assets/scripts/data_feeder.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+public class DataFeeder
+{
+    public Dictionary<int, Dictionary<string, Dictionary<string, float>>> acDataDict;
+    public int maxTimeNumber;
+    public DataFeeder(string filePath)
+    {
+        string rxPattern = @"\[(?<timestamp>.*?)\s.*\].*\n@N:(?<callsign>.*?):(?<squakcode>\d{4}):1:(?<latitude>.*?):(?<longitude>.*?):(?<altitude>\d*?|-\d*?):";
+        string esSecnarioText = System.IO.File.ReadAllText(filePath);
+        
+        Regex rx = new Regex(rxPattern);
+        MatchCollection matches = rx.Matches(esSecnarioText);
+        
+        // preprocessing for getting center point
+        List<string> timestampList = new List<string>();
+        List<string> callsignList = new List<string>();
+        List<float> latitudeList = new List<float>();
+        List<float> longitudeList = new List<float>();
+        List<float> altitudeList = new List<float>();
+        // custom time list
+        List<int> timeNumberList = new List<int>();
+        int timeNumber = 0;
+
+        int loopCount = 0;
+        foreach (Match match in matches)
+        {
+            GroupCollection matchGroups = match.Groups;
+            
+            timestampList.Add(matchGroups["timestamp"].Value);
+            callsignList.Add(matchGroups["callsign"].Value);
+            latitudeList.Add(float.Parse(matchGroups["latitude"].Value));
+            longitudeList.Add(float.Parse(matchGroups["longitude"].Value));
+            altitudeList.Add((float)(Double.Parse(matchGroups["altitude"].Value) * 3.281));
+
+            if (loopCount > 0 && timestampList[loopCount] != timestampList[loopCount - 1])
+            {
+                timeNumber++;
+            }
+            timeNumberList.Add(timeNumber);
+
+            loopCount++;
+        }
+        float maxLat = latitudeList.Max();
+        float minLat = latitudeList.Min();
+        float avgLat = latitudeList.Average();
+
+        float maxLon = longitudeList.Max();
+        float minLon = longitudeList.Min();
+        float avgLon = longitudeList.Average();
+
+        float maxAlt = altitudeList.Max();
+
+        
+        for (int i=0; i<timestampList.Count; i++)
+        {
+            latitudeList[i] = (latitudeList[i] - avgLat) * 100;
+            longitudeList[i] = (longitudeList[i] - avgLon) * 100;
+            altitudeList[i] = altitudeList[i] / 100;
+        }
+
+        // create dict for ac data
+        var dataDict = new Dictionary<int, Dictionary<string, Dictionary<string, float>>>();
+        for (int i = 0; i<timestampList.Count; i++)
+        {
+            try
+            {
+                dataDict[timeNumberList[i]][callsignList[i]] =
+                    new Dictionary<string, float>()
+                    {
+                        {"latitude", latitudeList[i]},
+                        {"longitude", longitudeList[i]},
+                        {"altitude", altitudeList[i]},
+                    };
+            }
+            catch (KeyNotFoundException)
+            {
+                dataDict[timeNumberList[i]] = new Dictionary<string, Dictionary<string, float>>()
+                {
+                    {
+                        callsignList[i],
+                        new Dictionary<string, float>()
+                        {
+                            // {"squakcode", matchGroups["squakcode"].Value},
+                            {"latitude", latitudeList[i]},
+                            {"longitude", longitudeList[i]},
+                            {"altitude", altitudeList[i]},
+                        }
+                    }
+                };
+            }
+        }
+
+        maxTimeNumber = timeNumberList.Max();
+        acDataDict = dataDict;
+        
+    }
+}
---
+++ Assets/scripts/run.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using SFB;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class run : MonoBehaviour
+{
+    
+    public GameObject prefab_ac;
+    public Slider timeNumberSlider;
+    
+    // Start is called before the first frame update
+    private Dictionary<int, Dictionary<string, Dictionary<string, float>>> dataDict;
+
+
+    private List<GameObject> acObjectList = new List<GameObject>();
+    void Start()
+    {
+        var fileFilterExtensions = new []
+        {
+            new ExtensionFilter("txt", "txt"),
+        };
+        string esSecnarioTextFilepath = StandaloneFileBrowser.OpenFilePanel("Open Euroscope Secnario File", "", fileFilterExtensions, false)[0];
+        Debug.Log(esSecnarioTextFilepath.ToString());
+        DataFeeder feeder = new DataFeeder(esSecnarioTextFilepath.ToString()); 
+        dataDict = feeder.acDataDict;
+        int maxTimeNumber = feeder.maxTimeNumber;
+
+        timeNumberSlider.maxValue = maxTimeNumber;
+        timeNumberSlider.value = 0;
+        
+        // // create from prefab
+        // var acDataDict = dataDict[0];
+        // foreach (var acEntry in acDataDict)
+        // {
+        //     var acEntryValue = acEntry.Value;
+        //     GameObject aircraft = Instantiate(prefab_ac, new Vector3(acEntryValue["latitude"], acEntryValue["altitude"], acEntryValue["longitude"]),
+        //         Quaternion.identity);
+        //     aircraft.name = acEntry.Key;
+        // }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var ac in acObjectList)
+        {
+            Destroy(ac);
+        }
+        acObjectList.Clear();
+        int currentTimeSelected = (int)timeNumberSlider.value;
+        
+        var acDataDict = dataDict[currentTimeSelected];
+        foreach (var acEntry in acDataDict)
+        {
+            var acEntryValue = acEntry.Value;
+            GameObject aircraft = Instantiate(prefab_ac, new Vector3(acEntryValue["latitude"], acEntryValue["altitude"], acEntryValue["longitude"]),
+                Quaternion.identity);
+            aircraft.name = acEntry.Key;
+            acObjectList.Add(aircraft);
+        }
+
+    }
+}
